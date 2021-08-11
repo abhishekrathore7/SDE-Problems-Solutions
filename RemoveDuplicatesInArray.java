87d35288660dc48ba887e2d4fc8a33a4dc584e32
Abhishek Rathore <abhishekrathore325@gmail.com>
2021-08-11 23:13:02 +0530

    2-Pointer Problems

---
+++ RemoveDuplicatesInArray.java
@@ -0,0 +1,26 @@
+package sdeProblems;
+import java.util.*;
+
+public class RemoveDuplicatesInArray {
+	static int duplicates(int arr[],int n) {
+		if(n==0 || n==1)
+			return n;
+		int j=0;
+		for(int i=0;i<n-1;i++) {
+			if(arr[i] != arr[i+1])
+				arr[j++] = arr[i];
+		}
+		arr[j++] = arr[n-1];
+		return j;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+        int n = arr.length;
+        n = duplicates(arr, n);
+        for (int i=0; i<n; i++)
+           System.out.print(arr[i]+" ");
+
+	}
+
+}
